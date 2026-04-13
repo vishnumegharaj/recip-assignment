e5c3de0565187b24d4c19ce22ab5a821ef10803a
vishnu megharaj <vishnumegharaj87@gmail.com>
2026-04-13 17:32:22 +0530

    added more users to create bulk users in data.json file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
@@ -11,6 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '10mb' }));       
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use("/api/users", userRoute)
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
